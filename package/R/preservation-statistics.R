ce46d1f6507b08666322189d3d8791b5fb48ae32
Scott Ritchie <sritchie73@gmail.com>
2013-08-21 15:45:58 +1000

    Now compiles as a package

--- package/R/preservation-statistics.R
+++ package/R/preservation-statistics.R
@@ -1,16 +1,15 @@
-#' This file holds all the statistics to be calculated between networks
-#' 
-#' Note: come back and formally attribute the WGCNA package and authors.
-#' 
-#' @import biganalytics bigmemory
-
+# This file holds all the statistics to be calculated between networks
+# 
+# Note: come back and formally attribute the WGCNA package and authors.
+# 
 
 #' Calculate the meanAdj statistic of a module in a test network
 #' 
-#' To minimise calculation all edges not to be calculated as part of the mean 
-#' should be marked NA. For example, gene coexpression adjacency matrices from 
-#' WGCNA should have their diagonal replaced with NA. For sparse networks the 
-#' user will need to determine where NA or 0 is most appropriate.
+#' NOTE: Make sure to mark with NA edges that should not be included in the 
+#' calculation. This is done to minimise computation.
+#' For example, gene coexpression adjacency matrices from WGCNA should have 
+#' their diagonal replaced with NA. For sparse networks the user will need to 
+#' determine where NA or 0 is most appropriate.
 #' 
 #' @param testNet the adjacency matrix for the test network
 #' @param testIndices the indices on the nodes in the module
