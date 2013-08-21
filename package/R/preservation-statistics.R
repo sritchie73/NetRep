e6c1d67a647feb177719a70b3af146d2643c9e37
Scott Ritchie <sritchie73@gmail.com>
2013-08-21 15:13:00 +1000

    Added function to calculate meanAdj statistic.

--- package/R/preservation-statistics.R
+++ package/R/preservation-statistics.R
@@ -1,1 +1,20 @@
-# This file holds all the statistics to be calculated between networks
+#' This file holds all the statistics to be calculated between networks
+#' 
+#' Note: come back and formally attribute the WGCNA package and authors.
+#' 
+#' @import biganalytics bigmemory
+
+
+#' Calculate the meanAdj statistic of a module in a test network
+#' 
+#' To minimise calculation all edges not to be calculated as part of the mean 
+#' should be marked NA. For example, gene coexpression adjacency matrices from 
+#' WGCNA should have their diagonal replaced with NA. For sparse networks the 
+#' user will need to determine where NA or 0 is most appropriate.
+#' 
+#' @param testNet the adjacency matrix for the test network
+#' @param testIndices the indices on the nodes in the module
+#' @return the mean edge weight of the module in the test network
+meanAdj <- function(testNet, testIndices) {
+  mean(testNet[testIndices, testIndices], na.rm=TRUE)
+}
