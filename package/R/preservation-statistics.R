ab99bd0f3c8ecab1e1e3499f15a4499576b4884c
Scott Ritchie <sritchie73@gmail.com>
2013-08-22 13:06:04 +1000

    Updated documentation for meanAdj

--- package/R/preservation-statistics.R
+++ package/R/preservation-statistics.R
@@ -5,15 +5,22 @@
 
 #' Calculate the meanAdj statistic of a module in a test network
 #' 
-#' NOTE: Make sure to mark with NA edges that should not be included in the 
+#' Calculates the mean edge weight of the module in the test network.
+#' A high preservation score indicates preservation of the module's density.
+#' 
+#' @note Make sure to mark with NA edges that should not be included in the 
 #' calculation. This is done to minimise computation.
 #' For example, gene coexpression adjacency matrices from WGCNA should have 
 #' their diagonal replaced with NA. For sparse networks the user will need to 
 #' determine where NA or 0 is most appropriate.
 #' 
-#' @param testNet the adjacency matrix for the test network
-#' @param testIndices the indices on the nodes in the module
-#' @return the mean edge weight of the module in the test network
+#' @references Langfelder, Peter; Luo, Rui; Oldham, Michael C.; and Horvath, 
+#'    Steve. Is My Network Module Preserved and Reproducible?. PLoS 
+#'    Computational Biology, 2011.
+#' 
+#' @param testNet the adjacency matrix for the test network.
+#' @param testIndices the indices on the nodes in the module.
+#' @return The mean edge weight of the module in the test network.
 meanAdj <- function(testNet, testIndices) {
   mean(testNet[testIndices, testIndices], na.rm=TRUE)
 }
