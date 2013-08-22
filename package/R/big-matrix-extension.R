5fd6bae347c200bbacc537e382e2791dbcf4fb2a
Scott Ritchie <sritchie73@gmail.com>
2013-08-22 17:52:52 +1000

    diag methods for big.matrix

--- package/R/big-matrix-extension.R
+++ package/R/big-matrix-extension.R
@@ -1,16 +1,16 @@
 # Added functionality for the S4 class `big.matrix`
 
-setGeneric("diag<-")
+library(foreach)
+
 setMethod("diag<-", signature("big.matrix"), function(x, value) {
-  foreach(i = 1:ncol(x)) %do%{
+  foreach (i = 1:ncol(x)) %do% {
     x[i,i] <- value
   }
   x
 })
 
-setGeneric("diag")
 setMethod("diag", signature("big.matrix"), function(x) {
-  foreach(i = 1:ncol(x), .combine=c) %do%{
-    x[i,i] <- value
+  foreach (i = 1:ncol(x), .combine=c) %do% {
+    x[i,i]
   }
 })
