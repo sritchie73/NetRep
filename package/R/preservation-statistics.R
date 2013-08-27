4343eeccd415b0b6754678c98a17b89ef4f074a0
Scott Ritchie <sritchie73@gmail.com>
2013-08-27 22:14:42 +1000

    Fixed doc error. Added error checking to R wrapper function.

--- package/R/preservation-statistics.R
+++ package/R/preservation-statistics.R
@@ -13,11 +13,19 @@
 #'    Computational Biology, 2011.
 #' 
 #' @param adjacency Adjacency matrix to calculate meanAdj of module in.
-#' @param moduleIndicies Where the nodes in the module are located in the 
+#' @param moduleIndices Where the nodes in the module are located in the 
 #'  adjacency network.
 #' @param includeDiagonal Logical, if FALSE, diagonal values (edges between 
 #'  nodes and themselves) are not counted towards the mean edge weight.
 #' @return The mean edge weight of the module in the test network.
 meanAdj <- function(adjacency, moduleIndices, includeDiagonal) {
+  # Error check inputs before passing to C++
+  stopifnot(class(adjacency) == "big.matrix")
+  stopifnot(is.vector(moduleIndices))
+  stopifnot(class(moduleIndices) %in% c("numeric", "integer"))
+  stopifnot(class(includeDiagonal) == "logical")
+  stopifnot(length(includeDiagonal) == 1)
+  stopifnot(!is.na(includeDiagonal))
+  
   MeanAdj(adjacency@address, moduleIndices, includeDiagonal)
 }
