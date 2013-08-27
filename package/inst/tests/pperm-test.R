ff9940ae17664d851e8b5d1f789ee5aa0c0a4317
Scott Ritchie <sritchie73@gmail.com>
2013-08-27 14:27:56 +1000

    Unit tests for `pperm` functions. Bugs: qperm does not work properly.

---
+++ package/inst/tests/pperm-test.R
@@ -0,0 +1,33 @@
+context("Functions on Distributions from Permutation Testing")
+
+normData <- rnorm(n=10000)
+approx <- function(...) { equals(..., tolerance=0.1)}
+
+test_that("pperm approximates normal for n=10000", {
+  expect_that(pperm(normData, -1.644854), approx(pnorm(-1.644854)))
+  expect_that(pperm(normData, 1.644854), approx(pnorm(1.644854)))
+  expect_that(pperm(normData, -1.644854, lower.tail=FALSE), approx(pnorm(1.644854)))  
+  expect_that(pperm(normData, 1.644854, lower.tail=FALSE), approx(pnorm(-1.644854)))  
+  expect_that(pperm(normData, -1.644854, log.p=TRUE), approx(pnorm(-1.644854, log.p=TRUE)))
+})
+
+test_that("qperm returns similar pvalues to pnorm for n=10000", {
+  expect_that(qperm(normData, 0.05), approx(qnorm(0.05)))
+  expect_that(qperm(normData, 0.01), approx(qnorm(0.01)))
+  expect_that(qperm(normData, 0.95), approx(qnorm(0.95)))
+  #expect_that(qperm(normData, -0.05129329, log.p=TRUE), approx(qnorm(-0.05129329, log.p=TRUE)))
+  #expect_that(qperm(normData, -2.995732, log.p=TRUE), approx(qnorm(-2.995732, log.p=TRUE)))
+})
+
+test_that("qperm and pperm agree on p-values calculation", {
+  small <- sort(runif(4))
+  ps <- sapply(small, function(x) { pperm(small, x) })
+  qs <- sapply(ps, function(x) { qperm(small, x) })
+
+  expect_identical(small, qs)
+})
+
+test_that("qperm handles multiple returns correctly", {
+  sparse <- runif(4)
+  
+})
