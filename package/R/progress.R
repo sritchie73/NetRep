5646e3906f5a17cafe7eff3b2e2e80b1f6b31dd6
Scott Ritchie <sritchie73@gmail.com>
2014-07-16 13:44:09 +1000

    bugfix

--- package/R/progress.R
+++ package/R/progress.R
@@ -32,11 +32,12 @@ setupParProgressLogs <- function(chunk, nChunks, ind) {
   # something along the lines of: 
   #  Chunk N: |========                 | 30%
   # The 2 corresponds to ": "
+  width <- options("width")[[1]]
   if (nChunks > 1) {
-    progWidth = options("width") - ind*2 - nchar("Chunks ") - nchar(nChunks) - 2
+    progWidth = width - ind*2 - nchar("Chunks ") - nchar(nChunks) - 2
   } else {
     # If only one worker/core, no need to prepend with "Chunk N: "
-    progWidth = options("width") - ind*2 
+    progWidth = width - ind*2 
   }
   txtProgressBar(min, max, min, width=progWidth, style=3)
 }
