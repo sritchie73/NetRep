c6db781a1f79a682d7701cfa0dd089cd01e419a1
Scott Ritchie <sritchie73@gmail.com>
2014-07-21 14:39:11 +1000

    Added unit test to make sure undirected meanAdj calculation gives the expected result.

--- package/inst/tests/test-cpp-statistics.R
+++ package/inst/tests/test-cpp-statistics.R
@@ -9,13 +9,20 @@ spr <- adj
 spr[sample(1:100, 25)] <- NA
 sprPtr <- as.big.matrix(spr)
 
+# Symmetric Matrix
+sym <- adj
+diag(sym) <- NA
+sym[lower.tri(sym)] <- t(sym)[lower.tri(sym)]
+symPtr <- as.big.matrix(sym)
+
 test_that("MeanAdj implementation is correct", {
   mod <- sample(1:10, 4)
-  expect_equal(meanAdjR(adj, 1:10), meanAdj(adjPtr, 1:10))
-  expect_equal(meanAdjR(spr, 1:10), meanAdj(sprPtr, 1:10))
-  expect_equal(meanAdjR(adj, mod), meanAdj(adjPtr, mod))
-  expect_error(meanAdj(adjPtr, 0:1))
-  expect_error(meanAdj(adjPtr, 11:12))
+  expect_equal(meanAdjR(adj, 1:10), meanAdj(adjPtr, 1:10, FALSE))
+  expect_equal(meanAdjR(spr, 1:10), meanAdj(sprPtr, 1:10, FALSE))
+  expect_equal(meanAdjR(adj, mod), meanAdj(adjPtr, mod, FALSE))
+  expect_equal(meanAdjR(sym, mod), meanAdj(symPtr, mod, TRUE))
+  expect_error(meanAdj(adjPtr, 0:1, FALSE))
+  expect_error(meanAdj(adjPtr, 11:12, FALSE))
 })
 
 test_that("kIM implementation is correct", {
