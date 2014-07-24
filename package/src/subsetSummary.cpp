34a2eed5eda11c0b6e213abc9778eb6c92553f25
Scott Ritchie <sritchie73@gmail.com>
2014-07-24 12:05:59 +1000

    Realised I don't need the adjacency matrix for this calculation.

--- package/src/subsetSummary.cpp
+++ package/src/subsetSummary.cpp
@@ -7,12 +7,11 @@ using namespace Rcpp;
 
 //' Network subset eigenvector and proportion of variance explained in C++
 //' 
-//' @param pAdjacency SEXP container for the pointer to the adjacency matrix
-//' @param pDat SEXP container for the pointer to the data matrix that 
-//'   \code{pAdjacency} was constructed from.
-//' @param adjIndices indices of the network subset of interest in
-//'   \code{pAdjacency}.
-//' @param datIndices indices of the network subset of interest in \code{pDat}.
+//' 
+//' @param pDat SEXP container for the pointer to the data matrix used in 
+//'   network construction.
+//' @param subsetIndices indices of the network subset of interest in 
+//'   \code{pDat}.
 //' 
 //' @return
 //'  A list whose first element is the first eigenvector of the singular value
@@ -32,22 +31,13 @@ using namespace Rcpp;
 //'  
 // [[Rcpp::export]]
 List SvdProps(
-  SEXP pAdjacency, SEXP pDat, IntegerVector adjIndices, IntegerVector datIndices
+  SEXP pDat, IntegerVector subsetIndices
 ) {
-  XPtr<BigMatrix> xpAdj(pAdjacency);
   XPtr<BigMatrix> xpDat(pDat);
   
   // Make sure we're not indexing out of range.
-  if (is_true(any(adjIndices <= 0)) || 
-      is_true(any(adjIndices > xpAdj->ncol())) ||
-      is_true(any(adjIndices > xpAdj->nrow()))) {
-    throw std::out_of_range(
-      "Some of the requested indices for network subset are outside of the "
-      "given adjacency matrix."
-    );
-  }
-  if (is_true(any(datIndices <= 0)) || 
-      is_true(any(datIndices > xpDat->ncol()))) {
+  if (is_true(any(subsetIndices <= 0)) || 
+      is_true(any(subsetIndices > xpDat->ncol()))) {
     throw std::out_of_range(
       "Some of the requested indices for network subset are outside of the "
       "given data matrix."
