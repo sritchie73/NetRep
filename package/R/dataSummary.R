b9408847c7f428f3f0d099a211b399fc396c1e8e
Scott Ritchie <sritchie73@gmail.com>
2014-07-28 14:31:21 +1000

    Fixed documentation and reflowed commentt

--- package/R/dataSummary.R
+++ package/R/dataSummary.R
@@ -10,21 +10,20 @@
 #'  are the samples.
 #' @param scaledData a \code{\link[bigmemory]{big.matrix}}: containing 
 #'  \code{data} where each row (node) has been scaled. See 
-#'  \code{\link[=scale-cpp]{scaleBigMatrix}}.
+#'  \code{\link{scaleBigMatrix}}.
 #' @param subsetIndices a vector of integers giving the row indices for the 
 #'  network subset of interest in \code{data} and \code{scaledData}
 #' 
 #' @details
-#' This returned vector is the first right singular vector from a 
-#' \link[base][=svd]{singular value decomposition}. The vector returned may 
-#' differ from that obtained through R's \code{\link[base]{svd}} due to 
-#' different C/C++ libraries being used.
+#' This returned vector is the first right singular vector from a singular value
+#' decomposition. The vector returned may differ from that obtained through R's
+#' \code{\link[base]{svd}} due to different C/C++ libraries being used.
 #' 
-#' Only \code{\link[bigmemory]{big.matrix}} objects of \code{type = "double"}
+#' Only \code{\link[bigmemory]{big.matrix}} objects of \code{type = "double"} 
 #' can be used.
 #' 
-#' The \code{scaledData} is used to orient this vector so that its direction is
-#' in alignment with the underlying data, as the SVD will sometimes return a
+#' The \code{scaledData} is used to orient this vector so that its direction is 
+#' in alignment with the underlying data, as the SVD will sometimes return a 
 #' vector which is strongly negatively correlated, rather than positively 
 #' correlated, with the data.
 #' 
@@ -36,8 +35,16 @@
 #' message:
 #'  \code{error: svd_econ(): failed to converge}
 #' throw a warning, and return a list of \code{NA}s. 
-#'
-#' @param data
+#' 
+#' @references
+#'   \enumerate{
+#'     \item{
+#'       Langfelder, P., Luo, R., Oldham, M. C. & Horvath, S. \emph{Is my 
+#'       network module preserved and reproducible?} PLoS Comput. Biol. 
+#'       \strong{7}, e1001057 (2011). 
+#'     }
+#'  }
+#'  
 #' @export
 dataSummary <- function(data, scaledData, subsetIndices) {
   return(DataSummary(data@address, scaledData@address, subsetIndices))
