139529ecb2aeb5e76b4d653e7d6465da5cb18d36
Scott Ritchie <sritchie73@gmail.com>
2014-09-16 14:51:41 +1000

    fixed naming incosistencies and deleted extraneous permutation p-value functions.

--- package/R/pperm.R
+++ package/R/pperm.R
@@ -39,40 +39,25 @@
 #'       zero: calculating exact P-values when permutations are randomly drawn.}
 #'       Stat. Appl. Genet. Mol. Biol. \strong{9}, Article39 (2010). 
 #'     }
-#'     \item{
-#'       Knijnenburg, T. A., Wessels, L. F. A., Reinders, M. J. T. & Shmulevich, 
-#'       I. \emph{Fewer permutations, more accurate P-values}. Bioinformatics 
-#'       \strong{25}, i161-8 (2009). 
-#'     }
 #'   }
 #'  
 #' @param permuted vector of values making up the empirical distribution.
 #' @param subsetSize the size of the network subset the null distribution is 
 #'  drawn for.
 #' @param q vector of quantiles.
-#' @param p vector of probabilities.
-#' @param n number of observations. If \code{length(n) > 1}, the length is 
-#'  taken to be the number required. 
-#' @param log.p logicial; if TRUE, probabilities p are given as log(p)
-#' @param lower.tail logical; if TRUE (default), probabilities are 
+#' @param lowerTail logical; if TRUE (default), probabilities are 
 #'    \eqn{P[x \le x]} otherwise \eqn{P[X > x]}.   
 #'  
 #' @examples
 #'  # A contrived example. For large n these results will be the same as qnorm, 
 #'  # rnorm, pnorm.
 #'  normData <- rnorm(n=10000)
-#'  qperm(normData, 0.95) # should be approximately 1.644854
-#'  rperm(normData, 100) # should be similar to rnorm(100)
-#'    
 #'  pperm(normData, 1.644854, 1) # should be approximately 0.05
 #'  
 #' @aliases permutation permuted
-#' @name permutation
-NULL
-
 # @param tail.approx logical; if \code{TRUE}, use the tail approximation 
 #  algorithm to estimate extreme p-values (see details).
-#' @rdname permutation
+#' @name permutation
 #' @importFrom statmod permp
 #' @export
 pperm <- function(permuted, q, moduleSize, lowerTail=FALSE) {
@@ -87,52 +72,18 @@ pperm <- function(permuted, q, moduleSize, lowerTail=FALSE) {
   return(p.value)
 }
 
-#' @rdname permutation
-#' @export
-qperm <- function(permuted, p, log.p = FALSE) {
-  if (((p < 0) || (p > 1)) && (!log.p)) {
-    stop("Error: `p` must be between 0 and 1.")
-  } else if ((p > 0) && (log.p)) {
-    stop("Error: `log(p)` must be between -Inf and 0.")
-  }
-  
-  # Generate all possible quantiles and pick the closest one
-  nPermutations <- length(permuted)
-  if (log.p) {
-    p.possible <- log(0:(nPermutations - 1) + 1) - log(nPermutations + 1)
-  } else {
-    p.possible <- {0:(nPermutations - 1) + 1} / {nPermutations + 1} 
-  }
-  p.diff <- abs(p.possible - p)
-  # Index/ices of the closest p-values to the given one
-  p.index <- which(is.equal(p.diff, min(p.diff)))
-  if (length(p.index) > 1) {
-    warning("Specified quantile ", p, " is halfway between two points on the",
-            " permuted distribution!")
-  }
-  return(sort(permuted)[p.index])
-}
-
-
-#' @usage rperm(permuted, n)
-#' @rdname permutation
-#' @export
-rperm <- function(permuted, n) {
-  sample(permuted, size=n, replace=TRUE)
-}
-
 #' @description
-#'  \code{requiredPower}: If the tail approximation is not being used, how many
-#'  permutations do I need to be able to detect significance at a given
-#'  threshold \code{alpha}?
+#'  \code{requiredPower}:  how many permutations do I need to be able to detect
+#'  significance at a given threshold \code{alpha}?
 #' 
 #' @param alpha desired significance threshold.
 #' @return The minimum number of permutations required to detect any significant
-#'  associations at the provided \code{alpha}.
+#'  associations at the provided \code{alpha}. The minimum p-value will always
+#'  be smaller than \code{alpha}.
 #' @rdname permutation
 #' @export
 requiredPower <- function(alpha) {
-  1/(alpha) - 1
+  n <- 1/alpha
 }
 
 
@@ -152,18 +103,18 @@ requiredPower <- function(alpha) {
 #'  from.
 #' @param tail.approx logical; if \code{TRUE}, use the tail approximation 
 #'  algorithm to estimate extreme p-values (see details).
-#' @param lower.tail logical; if TRUE (default), probabilities are 
+#' @param lowerTail logical; if TRUE (default), probabilities are 
 #'  \eqn{P[x \le x]} otherwise \eqn{P[X > x]}.
 #' @param ... other arguments to pass to \code{\link[graphics]{plot}}. Note that
 #'   providing \code{xlab}, \code{ylab}, \code{xlim}, or \code{ylim} will result
 #'   in an error. 
 #' @importFrom graphics plot
 #' @export
-qqperm <- function(permuted, q, tfunc, tail.approx=TRUE, lower.tail=FALSE, ...) {
+qqperm <- function(permuted, q, tfunc, tail.approx=TRUE, lowerTail=FALSE, ...) {
   permuted <- sort(permuted)
-  expected <- do.call(tfunc, list(q=q, lower.tail=lower.tail))
+  expected <- do.call(tfunc, list(q=q, lowerTail=lowerTail))
   observed <- sapply(q, function(x) {
-    pperm(permuted, x, lower.tail=lower.tail, tail.approx=tail.approx)
+    pperm(permuted, x, lowerTail=lowerTail, tail.approx=tail.approx)
   })
   min <- min(c(-log10(expected), -log10(observed)))
   max <- max(c(-log10(expected), -log10(observed)))
@@ -172,7 +123,7 @@ qqperm <- function(permuted, q, tfunc, tail.approx=TRUE, lower.tail=FALSE, ...) 
   abline(0, 1)
   # Color p-values that have been estimated using the tail approximation method
   if (tail.approx) {
-    if (lower.tail) {
+    if (lowerTail) {
       tai <- which(q < permuted[11])
     } else {
       tai <- which(q > tail(permuted, 11)[1])
