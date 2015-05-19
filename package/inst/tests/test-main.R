5a2f34e38b2b04f6c95ee672d264c42a7af28638
Scott Ritchie <sritchie73@gmail.com>
2015-05-19 18:41:17 +1000

    updated unit tests for modulePreservation

--- package/inst/tests/test-main.R
+++ package/inst/tests/test-main.R
@@ -4,16 +4,17 @@ gn1 <- paste0("N_", 1:100)
 gn2 <- paste0("N_", seq(2, 200, length=100))
 
 coexpSets <- list(
-  as.bigMatrix(matrix(rnorm(100*100), 100, dimnames=list(gn1, gn1)), "tmp1"),
-  as.bigMatrix(matrix(rnorm(100*100), 100, dimnames=list(gn2, gn2)), "tmp2")
+  a=as.bigMatrix(matrix(rnorm(100*100), 100, dimnames=list(gn1, gn1)), "tmp1"),
+  b=as.bigMatrix(matrix(rnorm(100*100), 100, dimnames=list(gn2, gn2)), "tmp2")
 )
 adjSets <- coexpSets
 exprSets <- list(
-  as.bigMatrix(matrix(rnorm(50*100), 50, dimnames=list(NULL, gn1)), "tmp3"),
-  as.bigMatrix(matrix(rnorm(75*100), 75, dimnames=list(NULL, gn2)), "tmp4")
+  a=as.bigMatrix(matrix(rnorm(50*100), 50, dimnames=list(NULL, gn1)), "tmp3"),
+  b=as.bigMatrix(matrix(rnorm(75*100), 75, dimnames=list(NULL, gn2)), "tmp4")
 )
-moduleAssignments <- list(sample(1:7, 100, replace=TRUE), NULL)
+moduleAssignments <- list(a=sample(1:7, 100, replace=TRUE), b=NULL)
 names(moduleAssignments[[1]]) <- gn1
+
 test_that("Network properties function runs without error", {
   expect_is(
     networkProperties(
@@ -29,14 +30,14 @@ test_that("Network properties function runs without error", {
 
 test_that("Main routine runs and produces sane output", {
   res1 <- modulePreservation(
-    exprSets, coexpSets, adjSets, moduleAssignments, 1, 2, nPerm=10
+    exprSets, coexpSets, adjSets, moduleAssignments, 1, 2
   )
   expect_equal(dim(res1$nulls), c(7, 7, 10))
   expect_equal(dim(res1$observed), c(7, 7))
   expect_equal(dim(res1$p.values), c(7, 7))
   expect_equal(length(res1$propGenesPresent), 7)
   expect_equal(length(res1$nGenesPresent), 7)
-  res2 <- netRepMain(
+  res2 <- modulePreservation(
     NULL, coexpSets, adjSets, moduleAssignments, 1, 2, nPerm=10
   )
   expect_equal(dim(res2$nulls), c(7, 4, 10))
@@ -44,5 +45,12 @@ test_that("Main routine runs and produces sane output", {
   expect_equal(dim(res2$p.values), c(7, 4))
   expect_equal(length(res2$propGenesPresent), 7)
   expect_equal(length(res2$nGenesPresent), 7)
+  
+  moduleAssignments[[2]]<- sample(1:9, 100, replace=TRUE)
+  names(moduleAssignments[[2]]) <- gn2
+  res1 <- modulePreservation(
+    exprSets, coexpSets, rev(adjSets), moduleAssignments, "a", "b", nPerm=10,
+    includeModules=c(1,2,3)
+  )
 })
 unlink('tmp*')
