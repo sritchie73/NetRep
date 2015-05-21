b86521e587e1eb3951cae915cae550cd4fdfee66
Scott Ritchie <sritchie73@gmail.com>
2015-05-21 12:12:48 +1000

    a

--- package/R/module-plots.R
+++ package/R/module-plots.R
@@ -20,6 +20,9 @@
 #'   used when the coexpression between two genes is equal to -1, and the last
 #'   element should correspond to the color used when the coexpression between
 #'   two genes is equal to 1
+#' @param drawBorder logical; if \code{TRUE}, borders are drawn around the bars
+#'  in \code{plotModuleMembership}, \code{plotConnectivity}, and 
+#'  \code{plotSummaryExpression}.
 #' 
 #' @template api_inputs
 #' 
@@ -235,7 +238,6 @@ plotAdjacency <- function(
   ) 
 }
 
-#' @param drawBorder logical; if \code{TRUE}, borders are drawn for the bars.
 #' @rdname plotTopology
 #' @export
 plotModuleMembership <- function(
@@ -344,6 +346,131 @@ plotModuleMembership <- function(
   plotBar(
     MM, c(-1,1), moduleAssignments[[discovery]][geneOrder],
     ifelse(MM > 0, palette[2], palette[1]), drawBorder=drawBorder
+  )
+  
+  # Add axes if specified
+  if (missing(plotModuleNames))
+    plotModuleNames <- !missing(modules) && length(modules) > 1
+  mas <- moduleAssignments[[discovery]][geneOrder]
+  if (plotGeneNames) {
+    axis(
+      side=1, las=2, at=seq_along(geneOrder), labels=geneOrder, tick=FALSE,
+      line=-0.5
+    )
+  }
+  if (plotModuleNames) {
+    line <- ifelse(plotGeneNames, 4, -0.5)
+    axis(
+      side=1, las=1, 
+      at=getModuleMidPoints(mas),
+      labels=modules, line=line, tick=FALSE
+    )
+  }
+  mtext(main, cex=par("cex.main"), font=2)
+}
+
+#' @rdname plotTopology
+#' @export
+plotConnectivity <- function(
+  geneExpression=NULL, coexpression, adjacency, moduleAssignments, modules,
+  discovery=1, test=1, symmetric=FALSE, geneOrder="discovery", 
+  plotGeneNames=TRUE, plotModuleNames, main="Normalised Connectivity", 
+  palette="#feb24c", drawBorder=FALSE
+) {
+  if (class(main) != "character")
+    stop("'main' must be a characer vector")
+  
+  geneOrderArgs <- c("discovery", "test", "none")
+  geneOrder <- geneOrderArgs[pmatch(geneOrder, geneOrderArgs, nomatch=3)]
+  
+  # Temporary directory to store new bigMatrix objects in
+  tmp.dir <- paste0(".temp-objects", getUUID())
+  dir.create(tmp.dir, showWarnings=FALSE)
+  on.exit({
+    unlink(tmp.dir, recursive=TRUE)
+  }, add=TRUE)
+  
+  # Unify data structures and load in matrices
+  geneExpression <- unifyDS(dynamicMatLoad(geneExpression, backingpath=tmp.dir))
+  coexpression <- unifyDS(dynamicMatLoad(coexpression, backingpath=tmp.dir))
+  adjacency <- unifyDS(dynamicMatLoad(adjacency, backingpath=tmp.dir))
+  
+  # If module discovery has not been performed for all datasets, it may be
+  # easier for the user to provide a simplified list structure
+  if (missing(moduleAssignments))
+    modules <- "1"
+  moduleAssignments <- formatModuleAssignments(
+    moduleAssignments, discovery, length(coexpression), names(coexpression),
+    ncol(coexpression[[discovery]]), colnames(coexpression[[discovery]])
+  )
+  
+  # Sanity check input for consistency.
+  checkSets(
+    geneExpression, coexpression, adjacency, moduleAssignments, discovery, test
+  )
+  
+  # Get the module membership for each module in the test network.
+  props <- networkProperties(
+    geneExpression, coexpression, adjacency, moduleAssignments, modules, 
+    discovery, test
+  )
+  if (length(modules) == 1)
+    props <- list(props)
+  
+  # Now we will order the genes ourselves to prevent duplicate calls to 
+  # networkProperties, which can be quite slow.
+  if (geneOrder == "discovery" || (geneOrder == "test" && discovery != test)) {
+    # Ordering genes by the discovery network however means we have to calculate
+    # The network properties in the discovery network
+    geneOrder <- geneOrder(
+      geneExpression, coexpression, adjacency, moduleAssignments, modules,
+      discovery, discovery
+    ) 
+  } else if (geneOrder == "none") {
+    moduleOrder <- seq_along(props)
+    geneOrder <- foreach(mi = moduleOrder, .combine=c) %do% {
+      names(props[[mi]]$connectivity)
+    }
+  } else {
+    # order modules
+    moduleOrder <- 1
+    if (length(props) > 1) {
+      if (!is.null(geneExpression[[test]])) {
+        # Create a matrix of summary expression profiles to measure the similarity
+        seps <- matrix(
+          0, ncol=length(props), nrow=length(props[[1]]$summaryExpression)
+        )
+        colnames(seps) <- names(props)
+        for (mi in seq_along(props)) {
+          seps[,mi] <- props[[mi]]$summaryExpression
+        }
+        moduleOrder <- hclust(as.dist(1-cor(seps)))$order
+      } else {
+        warning(
+          "No gene expression provided, modules will be ordered as provided"
+        )
+        moduleOrder <- seq_along(props)
+      }
+    } 
+    
+    # order genes
+    geneOrder <- foreach(mi = moduleOrder, .combine=c) %do% {
+      names(sort(
+        props[[mi]]$connectivity, decreasing=TRUE, na.last=TRUE
+      ))
+    }
+  }
+  
+  # now build the (Normalised) Intramodular Connectivity vector
+  kIM <- foreach(mi = seq_along(props), .combine=c) %do% {
+    props[[mi]]$connectivity/max(na.omit(props[[mi]]$connectivity))
+  }
+  kIM <- kIM[geneOrder]
+  
+  # Plot bar chart
+  plotBar(
+    kIM, c(0,1), moduleAssignments[[discovery]][geneOrder],
+    palette, drawBorder=drawBorder
   )
   
   # Add axes if specified
