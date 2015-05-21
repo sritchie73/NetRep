a9137cc982187b803053916f801c51a2e7df2658
Scott Ritchie <sritchie73@gmail.com>
2015-05-21 11:02:26 +1000

    Changed argument orderBy to geneOrder, and changed documentation so that all topology plots have the same documentation. plotCoexpression now handles genes present in the discovery dataset but not in the test dataset

--- package/R/module-plots.R
+++ package/R/module-plots.R
@@ -1,16 +1,14 @@
-#' Plot the coexpression of one or more modules
+#' Plot a topological feature of network module
 #' 
-#' Plot the coexpression of specified modules in either the discovery dataset or
-#' another independent test dataset.  If multiple modules are specified, boxes
-#' will be drawn around the module assignments.
 #' 
 #' @param symmetric logical; if \code{TRUE} the coexpression will be plotted as
 #'  a symmetric heatmap, if \code{FALSE} it will be plotted as a triangular
 #'  heatmap.
-#' @param orderBy one of "discovery", "test", or "none". If "discovery" genes
-#'   are ordered by intramodular connectivity in the \code{discovery} dataset
-#'   (see \code{\link{geneOrder}}). If "test" genes are orderd by intramodular
-#'   connectivity in the test dataset. If "none" no ordering is applied.
+#' @param geneOrder one of "discovery", "test", "none", or a vector of gene 
+#'   names. If "discovery" genes are ordered by intramodular connectivity in the
+#'   \code{discovery} dataset (see \code{\link{geneOrder}}). If "test" genes are
+#'   orderd by intramodular connectivity in the test dataset. If "none" no 
+#'   ordering is applied.
 #' @param plotGeneNames logical; if \code{TRUE}, plot the gene names below the
 #'  heatmap.
 #' @param plotModuleNames logical; if \code{TRUE}, plot the module names below 
@@ -51,7 +49,7 @@
 #' ## Example 2: Plot coexpression of the first 10 genes, 
 #' ## without ordering them
 #' plotCoexpression(
-#'  geA[,1:10], coexpA[1:10, 1:10], adjA[1:10, 1:10], orderBy="none"
+#'  geA[,1:10], coexpA[1:10, 1:10], adjA[1:10, 1:10], geneOrder="none"
 #' )
 #' 
 #' ## Example 3: Plot the coexpression of two adipose modules in the liver
@@ -113,19 +111,20 @@
 #' unlink("*_bm*") 
 #' }
 #' 
-#' @rdname plotCoexpression
+#' @rdname plotTopology
 #' @export
 plotCoexpression <- function(
   geneExpression=NULL, coexpression, adjacency, moduleAssignments, modules,
-  discovery=1, test=1, symmetric=FALSE, orderBy="discovery", 
+  discovery=1, test=1, symmetric=FALSE, geneOrder="discovery", 
   plotGeneNames=TRUE, plotModuleNames, main="Coexpression", 
   palette=coexpression.palette()
 ) {
   if (class(main) != "character")
     stop("'main' must be a characer vector")
-  orderByArgs <- c("discovery", "test", "none")
-  orderBy <- orderByArgs[pmatch(orderBy, orderByArgs, nomatch=3)]
-  
+
+  geneOrderArgs <- c("discovery", "test", "none")
+  geneOrder <- geneOrderArgs[pmatch(geneOrder, geneOrderArgs, nomatch=3)]
+
   # Temporary directory to store new bigMatrix objects in
   tmp.dir <- paste0(".temp-objects", getUUID())
   dir.create(tmp.dir, showWarnings=FALSE)
@@ -152,31 +151,33 @@ plotCoexpression <- function(
     geneExpression, coexpression, adjacency, moduleAssignments, discovery, test
   )
   
-  if (orderBy != "none") {
-    geneOrder <- geneOrder(
-      geneExpression, coexpression, adjacency, moduleAssignments, modules,
-      discovery, test=ifelse(orderBy == "discovery", discovery, test)
-    )
-  } else {
+  if (geneOrder == "none") {
     geneOrder <- getGenes(
       geneExpression, coexpression, adjacency, moduleAssignments, modules,
       discovery, discovery
     )
+  } else {
+    geneOrder <- geneOrder(
+      geneExpression, coexpression, adjacency, moduleAssignments, modules,
+      discovery, test=ifelse(geneOrder == "discovery", discovery, test)
+    )
   }
-
  
   if (missing(plotModuleNames))
     plotModuleNames <- !missing(modules) && length(modules) > 1
   
+  # Handle genes not present in the test dataset
+  na.pos <- which(geneOrder %nin% colnames(coexpression[[test]]))
+  presentGenes <- geneOrder[-na.pos]
   if (symmetric) {
     plotSquareHeatmap(
-      coexpression[[test]][geneOrder, geneOrder], palette, vlim=c(-1, 1),
-      moduleAssignments[[discovery]][geneOrder]
+      coexpression[[test]][presentGenes, presentGenes], palette, vlim=c(-1, 1),
+      moduleAssignments[[discovery]][presentGenes], na.pos
     )
   } else {
     plotTriangleHeatmap(
-      coexpression[[test]][geneOrder, geneOrder], palette, vlim=c(-1, 1),
-      moduleAssignments[[discovery]][geneOrder]
+      coexpression[[test]][presentGenes , presentGenes], palette, vlim=c(-1, 1),
+      moduleAssignments[[discovery]][presentGenes], na.pos
     )
   }
   
@@ -198,132 +199,19 @@ plotCoexpression <- function(
   mtext(main, cex=par("cex.main"), font=2)
 }
 
-#' Plot the adjacency of one or more modules
-#' 
-#' Plot the coexpression of specified modules in either the discovery dataset or
-#' another independent test dataset.  If multiple modules are specified, boxes
-#' will be drawn around the module assignments.
-#' 
-#' @param symmetric logical; if \code{TRUE} the coexpression will be plotted as
-#'  a symmetric heatmap, if \code{FALSE} it will be plotted as a triangular
-#'  heatmap.
-#' @param orderBy one of "discovery", "test", or "none". If "discovery" genes
-#'   are ordered by intramodular connectivity in the \code{discovery} dataset
-#'   (see \code{\link{geneOrder}}). If "test" genes are orderd by intramodular
-#'   connectivity in the test dataset. If "none" no ordering is applied.
-#' @param plotGeneNames logical; if \code{TRUE}, plot the gene names below the
-#'  heatmap.
-#' @param plotModuleNames logical; if \code{TRUE}, plot the module names below 
-#'   the heatmap. By default, module names are only plotted if multiple
-#'   \code{modules} are provided.
-#' @param main title for the plot.
-#' @param palette a vector of colors to interpolate over when plotting the 
-#'   coexpression. The first element should correspond to the color
-#'   used when the coexpression between two genes is equal to -1, and the last
-#'   element should correspond to the color used when the coexpression between
-#'   two genes is equal to 1
-#' 
-#' @template api_inputs
-#' 
-#' @examples
-#' \dontrun{
-#' ## Example 1: Plot coexpression of a single module
-#' 
-#' # First we need some example data
-#' geA <- matrix(rnorm(50*100), ncol=100) # gene expression
-#' colnames(geA) <- paste0("Gene_", 1:100)
-#' rownames(geA) <- paste0("CohortA_", 1:50)
-#' coexpA <- cor(geA) # coexpression
-#' adjA <- abs(coexpA)^5 # adjacency
-#' moduleAssignments <- sample(1:7, size=100, replace=TRUE)
-#' names(moduleAssignments) <- paste0("Gene_", 1:100)
-#' 
-#' # Create bigMatrix objects for each matrix.
-#' geA <- as.bigMatrix(geA, "geA_bm")
-#' coexpA <- as.bigMatrix(coexpA, "coexpA_bm")
-#' adjA <- as.bigMatrix(adjA, "adjA_bm")
-#' 
-#' # Plot coexpression of module 2
-#' plotAdjacency(
-#'   geA, coexpA, adjA, moduleAssignments, modules="2"
-#' )
-#' 
-#' ## Example 2: Plot coexpression of the first 10 genes, 
-#' ## without ordering them
-#' plotAdjacency(
-#'  geA[,1:10], coexpA[1:10, 1:10], adjA[1:10, 1:10], orderBy="none"
-#' )
-#' 
-#' ## Example 3: Plot the coexpression of two adipose modules in the liver
-#' ## dataset
-#' 
-#' geAdipose <- matrix(rnorm(50*100), ncol=100) # gene expression
-#' colnames(geAdipose) <- paste0("Gene_", 1:100)
-#' rownames(geAdipose) <- paste0("Sample_", 1:50)
-#' coexpAdipose <- cor(geAdipose) # coexpression
-#' adjAdipose <- abs(coexpAdipose)^5 # adjacency
-#' adiposeModules <- sample(0:7, size=100, replace=TRUE)
-#' names(adiposeModules) <- paste0("Gene_", 1:100)
-#' 
-#' geLiver <- matrix(rnorm(50*100), ncol=100) # gene expression
-#' colnames(geLiver) <- paste0("Gene_", 1:100)
-#' rownames(geLiver) <- paste0("Sample_", 1:50)
-#' coexpLiver <- cor(geLiver) # coexpression
-#' adjLiver <- abs(coexpLiver)^6 # adjacency
-#' liverModules <- sample(0:12, size=100, replace=TRUE)
-#' names(liverModules) <- paste0("Gene_", 1:100)
-#'
-#' geHeart <- matrix(rnorm(50*100), ncol=100) # gene expression
-#' colnames(geHeart) <- paste0("Gene_", 1:100)
-#' rownames(geHeart) <- paste0("Sample_", 1:50)
-#' coexpHeart <- cor(geHeart) # coexpression
-#' adjHeart <- abs(coexpHeart)^4 # adjacency
-#' heartModules <- sample(0:5, size=100, replace=TRUE)
-#' names(heartModules) <- paste0("Gene_", 1:100)
-#' 
-#' # Store each input type as a list, where each element corresponds
-#' # to one of the datasets
-#' geneExpression <- list(
-#'   adipose=as.bigMatrix(geAdipose, "geAdipose_bm"),
-#'   liver=as.bigMatrix(geLiver, "geLiver_bm"),  
-#'   heart=as.bigMatrix(geHeart, "geHeart_bm") 
-#' )
-#' coexpression <- list(
-#'   adipose=as.bigMatrix(coexpAdipose, "coexpAdipose_bm"),
-#'   liver=as.bigMatrix(coexpLiver, "coexpLiver_bm"),  
-#'   heart=as.bigMatrix(coexpHeart, "coexpHeart_bm") 
-#' )
-#' adjacency <- list(
-#'   adipose=as.bigMatrix(adjAdipose, "adjAdipose_bm"),
-#'   liver=as.bigMatrix(adjLiver, "adjLiver_bm"),  
-#'   heart=as.bigMatrix(adjHeart, "adjHeart_bm") 
-#' )
-#' moduleAssignments <- list(
-#'   adipose=adiposeModules, liver=liverModules, heart=heartModules
-#' )
-#' 
-#' # Plot coexpression for adipose modules 3 and 7 in the heart tissue.
-#' plotAdjacency(
-#'   geneExpression, coexpression, adjacency, moduleAssignments,
-#'   modules=c("3", "7"), discovery="adipose", test="heart",
-#'   main="Heart", plotGeneNames=FALSE
-#' )
-#' 
-#' # clean up bigMatrix files from examples
-#' unlink("*_bm*") 
-#' }
-#' 
-#' @rdname plotAdjacency
+#' @rdname plotTopology
 #' @export
 plotAdjacency <- function(
   geneExpression=NULL, coexpression, adjacency, moduleAssignments, modules,
-  discovery=1, test=1, symmetric=FALSE, orderBy="discovery", 
+  discovery=1, test=1, symmetric=FALSE, geneOrder="discovery", 
   plotGeneNames=TRUE, plotModuleNames, main="Adjacency", 
   palette=coexpression.palette()
 ) {
   plotCoexpression(
     geneExpression, adjacency, adjacency, moduleAssignments, modules,
-    discovery, test, symmetric, orderBy, plotGeneNames, plotModuleNames,
+    discovery, test, symmetric, geneOrder, plotGeneNames, plotModuleNames,
     main, palette
   ) 
+}
+
 }
