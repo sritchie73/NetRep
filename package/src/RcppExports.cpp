9ee4481e8a65277215e26c57d9f0961d7ac69c13
Scott Ritchie <sritchie73@gmail.com>
2015-05-26 18:12:56 +1000

    Rcpp doing its thing

--- package/src/RcppExports.cpp
+++ package/src/RcppExports.cpp
@@ -18,6 +18,16 @@ BEGIN_RCPP
     return __result;
 END_RCPP
 }
+// CheckFinite
+void CheckFinite(SEXP pDat);
+RcppExport SEXP fastModPres_CheckFinite(SEXP pDatSEXP) {
+BEGIN_RCPP
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< SEXP >::type pDat(pDatSEXP);
+    CheckFinite(pDat);
+    return R_NilValue;
+END_RCPP
+}
 // CoexpStats
 List CoexpStats(SEXP pCoexpD, IntegerVector discIndices, SEXP pCoexpT, IntegerVector testIndices);
 RcppExport SEXP fastModPres_CoexpStats(SEXP pCoexpDSEXP, SEXP discIndicesSEXP, SEXP pCoexpTSEXP, SEXP testIndicesSEXP) {
@@ -29,6 +39,18 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< SEXP >::type pCoexpT(pCoexpTSEXP);
     Rcpp::traits::input_parameter< IntegerVector >::type testIndices(testIndicesSEXP);
     __result = Rcpp::wrap(CoexpStats(pCoexpD, discIndices, pCoexpT, testIndices));
+    return __result;
+END_RCPP
+}
+// DataProps
+List DataProps(SEXP pDat, IntegerVector subsetIndices);
+RcppExport SEXP fastModPres_DataProps(SEXP pDatSEXP, SEXP subsetIndicesSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< SEXP >::type pDat(pDatSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type subsetIndices(subsetIndicesSEXP);
+    __result = Rcpp::wrap(DataProps(pDat, subsetIndices));
     return __result;
 END_RCPP
 }
@@ -52,28 +74,6 @@ BEGIN_RCPP
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< SEXP >::type pDat(pDatSEXP);
     __result = Rcpp::wrap(BigRange(pDat));
-    return __result;
-END_RCPP
-}
-// CheckFinite
-void CheckFinite(SEXP pDat);
-RcppExport SEXP fastModPres_CheckFinite(SEXP pDatSEXP) {
-BEGIN_RCPP
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< SEXP >::type pDat(pDatSEXP);
-    CheckFinite(pDat);
-    return R_NilValue;
-END_RCPP
-}
-// DataProps
-List DataProps(SEXP pDat, IntegerVector subsetIndices);
-RcppExport SEXP fastModPres_DataProps(SEXP pDatSEXP, SEXP subsetIndicesSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< SEXP >::type pDat(pDatSEXP);
-    Rcpp::traits::input_parameter< IntegerVector >::type subsetIndices(subsetIndicesSEXP);
-    __result = Rcpp::wrap(DataProps(pDat, subsetIndices));
     return __result;
 END_RCPP
 }
