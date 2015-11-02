7b9a759aa16f07229e21f695762145ab3491f3b6
Scott Ritchie <sritchie73@gmail.com>
2015-11-02 16:13:16 +1100

    old changes

--- R/plot-utils.R
+++ R/plot-utils.R
@@ -2,19 +2,19 @@
 #'
 #' For a color gradient mapped onto a scale of values (\code{vlim}), retrieve
 #' the color for any given \code{value}
-#' 
+#'
 #' @param values a vector of values to retrieve the colors for.
 #' @param palette a vector of colors
 #' @param vlim limits of the values
-#' 
+#'
 #' @return
-#'  The color for a given value. 
+#'  The color for a given value.
 getColFromPalette <- function(values, palette, vlim) {
   if (missing(vlim)) {
-    vlim <- range(values)                                                                                                                                                                                                                     
+    vlim <- range(values)
   }
   vpal <- seq(vlim[1], vlim[2], length=length(palette)+1)
-  
+
   sapply(values, function(vv) {
     if (vv >= vlim[2]) {
       tail(palette, n=1)
@@ -22,8 +22,8 @@ getColFromPalette <- function(values, palette, vlim) {
       palette[1]
     } else {
       palette[sum(vv > vpal)]
-    }   
-  })  
+    }
+  })
 }
 
 #' Get the plot limits to set for the desired plot window
@@ -32,10 +32,10 @@ getColFromPalette <- function(values, palette, vlim) {
 #' 'xlim' and 'ylim', amounting to 4% of the plot region in either direction.
 #' This function tells you what limits to set so that the boundaries of the plot
 #' are precisely the min and max you want.
-#' 
+#'
 #' @param dlim the limits you want to set
 #' @return
-#'  the min and max to set for the desired plot limits  
+#'  the min and max to set for the desired plot limits
 forceLim <- function(dlim) {
   A = matrix(c(1.04, -0.04, -0.04, 1.04), nrow=2, ncol=2)
   B = matrix(dlim, nrow=2, ncol=1)
@@ -43,15 +43,15 @@ forceLim <- function(dlim) {
 }
 
 #' Create an empty plot window with the specified limits
-#' 
+#'
 #' @param ... extra arguments to pass to \code{\link[graphics]{plot}}.
 #' @param xlim limits for the x axis.
 #' @param ylim limits for the y axis.
 #' @param xlab label for the x axis.
 #' @param ylab label for the y axis.
-#' @param hardlim logical; if \code{TRUE}, the plot window is exactly 
+#' @param hardlim logical; if \code{TRUE}, the plot window is exactly
 #'  constrained to the specified \code{xlim} and \code{ylim}.
-#'  
+#'
 emptyPlot <- function(..., xlim, ylim, xlab="", ylab="", hardlim=TRUE) {
   if (!missing(xlim) & !missing(ylim)) {
     if(hardlim) {
@@ -59,7 +59,7 @@ emptyPlot <- function(..., xlim, ylim, xlab="", ylab="", hardlim=TRUE) {
       ylim <- forceLim(ylim)
     }
     plot(
-      ..., 0, type='n', xaxt='n', yaxt='n', xlim=xlim, ylim=ylim, xlab=xlab, 
+      ..., 0, type='n', xaxt='n', yaxt='n', xlim=xlim, ylim=ylim, xlab=xlab,
       ylab=ylab
     )
   } else if (!missing(ylim)) {
@@ -84,15 +84,15 @@ emptyPlot <- function(..., xlim, ylim, xlab="", ylab="", hardlim=TRUE) {
 }
 
 #' Get the hexidecimal value for a named color
-#' 
+#'
 #' @param name the color name
 #' @return the hex value for that color
-col2hex <- function(name) { 
-  paste0("#", paste(as.hexmode(col2rgb(name)), collapse="")) 
+col2hex <- function(name) {
+  paste0("#", paste(as.hexmode(col2rgb(name)), collapse=""))
 }
 
-#' Adds alpha transparency to any color 
-#' 
+#' Adds alpha transparency to any color
+#'
 #' @param col color
 #' @param alpha alpha transparency, should be between 0 and 1
 #' @return the hexidecimal value of the color with the added alpha channel
@@ -104,8 +104,8 @@ addAlpha <- function(col, alpha) {
 }
 
 #' Color palette for coexpression heatmaps
-#' 
-#' RColorBrewer palette "RdYlBu" with the middle color replaced with white. 
+#'
+#' RColorBrewer palette "RdYlBu" with the middle color replaced with white.
 #' This gives a nicer contrast than the "RdBu" palette
 coexpression.palette <- function() {
   cols <- rev(brewer.pal(11, "RdYlBu"))
@@ -114,16 +114,16 @@ coexpression.palette <- function() {
 }
 
 #' Color palette for adjacency heatmaps
-#' 
-#' RColorBrewer palette "RdYlBu" with the middle color replaced with white. 
+#'
+#' RColorBrewer palette "RdYlBu" with the middle color replaced with white.
 adjacency.palette <- function() {
   coexpression.palette()[6:11]
 }
 
 #' Color palette for expression and summary expression plots
-#' 
+#'
 #' RColorBrewer palette "PRGn" with a white middle color.
-#' 
+#'
 #' @import RColorBrewer
 expression.palette <- function() {
   cols <- brewer.pal(9, "PRGn")
@@ -132,10 +132,10 @@ expression.palette <- function() {
 }
 
 #' Get module break points on the x-axis
-#' 
+#'
 #' @param mas ordered subset of the moduleAssignments vector
-#' 
-#' @return 
+#'
+#' @return
 #'  a vector of positions on the x-axis where one module begins and another ends
 getModuleBreaks <- function(mas) {
   sizes <- rle(mas)
@@ -148,10 +148,10 @@ getModuleBreaks <- function(mas) {
 }
 
 #' Get module mid-points on the x-axis
-#' 
+#'
 #' @param mas ordered subset of the moduleAssignments vector
-#' 
-#' @return 
+#'
+#' @return
 #'  a vector of positions on the x-axis indicating the centre of a module
 getModuleMidPoints <- function(mas) {
   breaks <- getModuleBreaks(mas)
