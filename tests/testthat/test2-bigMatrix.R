0ec8687ef0693bcc0cf9e0eff6532e6e7ad4f7e4
Scott Ritchie <sritchie73@gmail.com>
2016-02-18 15:14:55 +1100

    bigMatrix tests now work

--- tests/testthat/test2-bigMatrix.R
+++ tests/testthat/test2-bigMatrix.R
@@ -1,6 +1,9 @@
 context("Testing bigMatrix implementation")
 suppressMessages(library(bigmemory))
 
+tmp.dir <- file.path(tempdir(), paste0(".NetRep", getUUID()))
+dir.create(tmp.dir, showWarnings=FALSE)
+
 # Test implementation of `[` with every combination or arguments and matrix types
 sampleTF <- function(n) {
   c(TRUE, FALSE, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE)[seq_len(n)]
@@ -20,8 +23,8 @@ for (mm in 1:3) { # Matrix, single column, and single row behave differently
       "2" = { dimnames(m1) <- list(letters[seq_len(nrow(m1))], NULL) },
       "3" = { dimnames(m1) <- list(NULL, LETTERS[seq_len(ncol(m1))]) },
       "4" = { dimnames(m1) <- list(letters[seq_len(nrow(m1))], LETTERS[seq_len(ncol(m1))]) })
-    save.as.bigMatrix(m1, file.path(tempdir(), "tmp1"), type="integer")
-    d1 <- load.bigMatrix(file.path(tempdir(), "tmp1"))
+    save.as.bigMatrix(m1, file.path(tmp.dir, "tmp1"), type="integer")
+    d1 <- load.bigMatrix(file.path(tmp.dir, "tmp1"))
     for (ii in 1:4) {
       # We need to handle cases where there is only 1 row
       switch(ii,
@@ -48,19 +51,19 @@ for (mm in 1:3) { # Matrix, single column, and single row behave differently
     d2 <- as.big.matrix(d1)
     expect_identical(d2[,], d1[,])
     # Test loading from a "big.matrix"
-    d3 <- load.bigMatrix(file.path(tempdir(), "tmp1"))
+    d3 <- load.bigMatrix(file.path(tmp.dir, "tmp1"))
     expect_identical(d3, d1)
     
     # Check reading and writing
     write.bigMatrix(
-      d1, file.path(tempdir(), "tmp1.txt"), 
+      d1, file.path(tmp.dir, "tmp1.txt"), 
       sep="\t", quote=FALSE, 
       row.names=ifelse(is.null(rownames(d1)), FALSE, TRUE),
       col.names=ifelse(is.null(colnames(d1)), FALSE, TRUE)
     )
     d5 <- read.bigMatrix(
-      file.path(tempdir(), "tmp1.txt"), 
-      file.path(tempdir(), "tmp1-1"), 
+      file.path(tmp.dir, "tmp1.txt"), 
+      file.path(tmp.dir, "tmp1-1"), 
       type="integer", sep="\t",
       row.names=ifelse(is.null(rownames(d1)), FALSE, TRUE),
       header=ifelse(is.null(colnames(d1)), FALSE, TRUE)
@@ -69,4 +72,4 @@ for (mm in 1:3) { # Matrix, single column, and single row behave differently
   }
 }
 
-cleanTempDir()
+unlink(tmp.dir, recursive=TRUE)
