ef47429d53923a1aaf3579f178ca86548a7726fd
Scott Ritchie <sritchie73@gmail.com>
2016-02-24 01:10:29 +1100

    Test-main now does not generate warnings

--- tests/testthat/test4-main.R
+++ tests/testthat/test4-main.R
@@ -1,5 +1,5 @@
 context("Testing 'modulePreservation' function")
-set.seed(1000)
+set.seed(37)
 gn1 <- paste0("N_", 1:100)
 gn2 <- paste0("N_", seq(2, 200, length=100))
 
@@ -27,9 +27,15 @@ exprSets <- list(
 moduleAssignments <- list(a=sample(1:7, 100, replace=TRUE), b=NULL)
 names(moduleAssignments[[1]]) <- gn1
 
+# Only analyse modules with > 2 genes
+modules <- moduleAssignments[[1]][intersect(names(moduleAssignments[[1]]), 
+                                            colnames(adjSets[[2]]))]
+modules <- table(modules)
+modules <- names(modules[modules > 2])
+
 test_that("Main routine runs and produces sane output", {
   res1 <- modulePreservation(
-    exprSets, coexpSets, adjSets, moduleAssignments, 
+    exprSets, coexpSets, adjSets, moduleAssignments, modules,
     discovery=1, test=2, nPerm=10, verbose=FALSE, nCores=1
   )
   expect_equal(dim(res1$nulls), c(7, 7, 10))
@@ -38,9 +44,9 @@ test_that("Main routine runs and produces sane output", {
   expect_equal(length(res1$propVarsPresent), 7)
   expect_equal(length(res1$nVarsPresent), 7)
   res2 <- modulePreservation(
-    NULL, coexpSets, adjSets, moduleAssignments, 
-    discovery=1, test=2, nPerm=10, verbose=FALSE, 
-    nCores=1
+    NULL, coexpSets, adjSets, moduleAssignments,
+    modules, discovery=1, test=2, nPerm=10, 
+    verbose=FALSE, nCores=1
   )
   expect_equal(dim(res2$nulls), c(7, 4, 10))
   expect_equal(dim(res2$observed), c(7, 4))
@@ -50,7 +56,7 @@ test_that("Main routine runs and produces sane output", {
   
   res1 <- modulePreservation(
     exprSets, coexpSets, adjSets, moduleAssignments, 
-    discovery="a", test="b", nPerm=10, modules=c(1,2,3), 
+    modules, discovery="a", test="b", nPerm=10,
     verbose=FALSE, nCores=1
   )
 })
